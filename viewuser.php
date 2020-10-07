8168d5aa88090f9fe25cb951a6b857fa12f5d26d
manchala meherish <manchala.meherish1524@gmail.com>
2020-10-07 15:06:41 +0530

    Update viewuser.php

--- viewuser.php
+++ viewuser.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-$con=mysqli_connect('localhost','root','','company');
+$con=mysqli_connect('remotemysql.com','3RPLsjVSDB','X2opJ6FljH','3RPLsjVSDB');
 //mysqli_select_db($con,'id8930489_spark');
 $name1=$_SESSION['name'];
 $q="select name from user where not name='$name1'";
